231c028967a0185a967aa8d42685ec5dc3f24a41
baibolot <azhybekuulu96@bk.ru>
2021-02-15 16:28:07 +0900

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+def add(x, y):
+
+   return x + y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
