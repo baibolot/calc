792eb750d95492a0c2951ab52ccaee0ed82076c0
baibolot <azhybekuulu96@bk.ru>
2021-02-15 17:03:09 +0900

    ubtract

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ def add(x, y):
    return x + y
 
 def ubtract(x, y):
-   """"ubtract"""
+    """"ubtract"""
    return x - y
 
 def multiply(x, y):
-
+    """"ubtract"""
    return x * y
 
 def divide(x, y):
